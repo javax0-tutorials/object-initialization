d2af04c1f75f3a5b2ab82e3c938fbb31305055ce
Peter Verhas <peter@verhas.com>
2017-03-08 21:53:23 +0100

    main was added

---
+++ src/com/javax0/classinit/Main.java
@@ -0,0 +1,15 @@
+package com.javax0.classinit;
+
+import static com.javax0.classinit.Printer.println;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        println("creating concrete object");
+        Concrete c = new Concrete();
+        println("1st concrete object done");
+        c = new Concrete();
+        c.x.toString();
+        println("2nd concrete object done");
+    }
+}
