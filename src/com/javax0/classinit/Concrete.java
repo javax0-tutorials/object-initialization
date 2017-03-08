8c89a2c15e30d861a5bdf478f71bb2b4072ccc50
Peter Verhas <peter@verhas.com>
2017-03-08 21:23:38 +0100

    extra classes deleted and concrete has a static instance

--- src/com/javax0/classinit/Concrete.java
+++ src/com/javax0/classinit/Concrete.java
@@ -2,26 +2,28 @@ package com.javax0.classinit;
 
 import static com.javax0.classinit.Printer.println;
 
-public class Concrete extends Abstract {
+public class Concrete {
+    static Concrete me = new Concrete();
 
     static {
-        println("static block before Concrete constructor");
+        println("static block before constructor");
     }
 
+
     {
-        println("block before Concrete constructor");
+        println("block before constructor");
     }
 
     public Concrete() {
-        println("Concrete constructor");
+        println("constructor");
     }
 
     static {
-        println("static block after Concrete constructor");
+        println("static block after constructor");
     }
 
     {
-        println("block after Concrete constructor");
+        println("block after constructor");
     }
 
 }
