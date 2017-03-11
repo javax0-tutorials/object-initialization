777c275fb78978747a16c8b0a17266458f46f317
Peter Verhas <peter@verhas.com>
2017-03-11 21:28:29 +0100

    formatting

--- src/com/javax0/classinit/Printer.java
+++ src/com/javax0/classinit/Printer.java
@@ -2,7 +2,7 @@ package com.javax0.classinit;
 
 public class Printer {
 
-    public static void println(String message) {
+    public static void println( String message) {
         System.out.println(message);
     }
 
